8981b6380d1d32b08d1e92ee464a42a848b0f7a3
Emanuel Cino <ecino@compassion.ch>
2020-06-04 11:20:27 +0200

    [12.0][MIG] partner_tag_smart_assignation module

--- partner_tag_smart_assignation/migrations/11.0.1.0.0/pre-migration.py
+++
@@ -1,8 +0,0 @@
-from openupgradelib import openupgrade
-
-
-def migrate(cr, version):
-    if not version:
-        return
-    openupgrade.update_module_names(cr, [
-        ('smart_tagger', 'partner_tag_smart_assignation')])
